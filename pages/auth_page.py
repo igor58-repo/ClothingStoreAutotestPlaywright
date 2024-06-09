438e1f97cb0d6b0b4c7082ec4a7c6f4ef0f03a95
igor58-repo <emzot96@gmail.com>
2024-06-09 13:30:02 +0300

    added authorization negative tests

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -18,7 +18,7 @@ def auth_page_check_elements(page: Page):
     expect(login_input, f"Некорретный текст плейсхолдера поля ввода логина. ОР: 'Username', "
                         f"ФР: {login_input.get_attribute('placeholder')}").to_have_attribute('placeholder', 'Username')
     expect(password_input, f"Некорретный текст плейсхолдера поля ввода пароля. ОР: 'Password', "
-                        f"ФР: {password_input.get_attribute('placeholder')}").to_have_attribute('placeholder', 'Password')
+                    f"ФР: {password_input.get_attribute('placeholder')}").to_have_attribute('placeholder', 'Password')
     expect(login_button, f"Некорретный текст на кнопке Login. ОР: 'Login', "
                         f"ФР: {login_button.get_attribute('value')}").to_have_text('Login')
 
@@ -28,3 +28,29 @@ def authorization(page: Page, login, password):
     page.locator("#user-name").fill(login)
     page.locator("#password").fill(password)
     page.locator("#login-button").click()
+
+
+# проверка элементов страницы после неудачной авторизации
+def check_opened_failed_auth_elements(page: Page, expected_error_message):
+    error_message = page.locator("div.error>h3")
+    expect(page.locator("#user-name"),
+           "Поле ввода логина не выделено красным").to_have_class("input_error form_input error")
+    expect(page.locator("#password"),
+           "Поле ввода пароля не выделено красным").to_have_class("input_error form_input error")
+    expect(error_message, "Не отображается сообщение об ошибке").to_be_visible()
+    expect(page.locator("svg.error_icon"), "Не отображаются иконки напротив полей ввода").to_have_count(2)
+    expect(page.locator("button.error-button"), "Не отображается кнопка закрытия сообщения об ошибке").to_be_visible()
+    current_error_message = error_message.inner_text()
+    assert current_error_message == expected_error_message, \
+        f"Некорректный текст ошибки. ОР: {expected_error_message}, ФР: {current_error_message}"
+
+
+# закрыть сообщение о неудачной авторизации и проверить элементы страницы
+def close_failed_auth_elements(page: Page):
+    page.locator("button.error-button").click()
+    expect(page.locator("svg.error_icon"), "Иконки напротив полей ввода не должны отображаться").not_to_be_visible()
+    expect(page.locator("div.error"), "Сообщение об ошибке не должно отображаться").not_to_be_visible()
+    expect(page.locator("#user-name"),
+           "Поле ввода логина не должно быть выделено красным").not_to_have_class("input_error form_input error")
+    expect(page.locator("#password"),
+           "Поле ввода пароля не должно быть выделено красным").not_to_have_class("input_error form_input error")
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,14 @@
 import pytest
 from vars import url_auth_page, url_main_page
-from pages.auth_page import auth_page_check_elements, authorization
-from credentials import all_users
-from playwright.sync_api import Page, expect
+from pages.auth_page import (auth_page_check_elements, authorization, check_opened_failed_auth_elements,
+                             close_failed_auth_elements)
+from credentials import all_users, non_existent_user, locked_out_user, empty_login, empty_password
+from playwright.sync_api import Page
+
+error_message_non_existent_user = "Epic sadface: Username and password do not match any user in this service"
+error_message_locked_out_user = "Epic sadface: Sorry, this user has been locked out."
+error_message_empty_login = "Epic sadface: Username is required"
+error_message_empty_password = "Epic sadface: Password is required"
 
 
 @pytest.mark.auth
@@ -19,3 +25,43 @@ def test_authorization(page: Page, login, password):
     page.goto(url_auth_page)
     authorization(page, login, password)
     assert page.url == url_main_page, f"Некорректный адрес. ОР: {url_main_page}, ФР: {page.url}"
+
+
+@pytest.mark.auth
+@pytest.mark.parametrize(*non_existent_user)
+def test_authorization_non_existent_user(page: Page, login, password):
+    """Проверка авторизации несуществующим пользователем"""
+    page.goto(url_auth_page)
+    authorization(page, login, password)
+    check_opened_failed_auth_elements(page, error_message_non_existent_user)
+    close_failed_auth_elements(page)
+
+
+@pytest.mark.auth
+@pytest.mark.parametrize(*locked_out_user)
+def test_authorization_locked_out_user(page: Page, login, password):
+    """Проверка авторизации заблокированным пользователем"""
+    page.goto(url_auth_page)
+    authorization(page, login, password)
+    check_opened_failed_auth_elements(page, error_message_locked_out_user)
+    close_failed_auth_elements(page)
+
+
+@pytest.mark.auth
+@pytest.mark.parametrize(*empty_login)
+def test_authorization_empty_login(page: Page, login, password):
+    """Проверка авторизации с незаполненным полем Логина"""
+    page.goto(url_auth_page)
+    authorization(page, login, password)
+    check_opened_failed_auth_elements(page, error_message_empty_login)
+    close_failed_auth_elements(page)
+
+
+@pytest.mark.auth
+@pytest.mark.parametrize(*empty_password)
+def test_authorization_empty_password(page: Page, login, password):
+    """Проверка авторизации с незаполненным полем Пароль"""
+    page.goto(url_auth_page)
+    authorization(page, login, password)
+    check_opened_failed_auth_elements(page, error_message_empty_password)
+    close_failed_auth_elements(page)
