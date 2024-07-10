e3a5426cc21866badc75ca43e7a8cfbf318e7900
igor58-repo <emzot96@gmail.com>
2024-07-10 18:08:21 +0300

    refactoring finish page tests

--- pages/finish_page.py
+++ pages/finish_page.py
@@ -1,27 +1,31 @@
 import pytest
-from vars import header_text, final_title, complete_header_text, complete_text
+from vars import header_text, final_title, complete_header_text, complete_message
 from playwright.sync_api import Page, expect
 
 
-# проверка элементов страницы
-def check_elements(page: Page):
-    app_logo = page.locator("div.app_logo")
-    title = page.locator("span.title")
-    img = page.locator("img.pony_express")
-    complete_header = page.locator("h2.complete-header")
-    complete = page.locator("div.complete-text")
-    back_home_button = page.locator("button#back-to-products")
-    expect(app_logo, "Не отображается текст логотипа").to_be_visible()
-    expect(title, "Не отображается заголовок").to_be_visible()
-    expect(img, "Не отображается изображение").to_be_visible()
-    expect(complete_header, "Не отображается сообщение в заголовке").to_be_visible()
-    expect(complete, "Не отображается текст об успешном заказе").to_be_visible()
-    expect(back_home_button, "Не отображается кнопка Back Home").to_be_visible()
-    expect(app_logo, f"Некорректный текст логотипа. ОР: {header_text}, "
-                     f"ФР: {app_logo.inner_text()}").to_have_text(header_text)
-    expect(title, f"Некорректный текст заголовка. ОР: {final_title}, "
-                  f"ФР: {title.inner_text()}").to_have_text(final_title)
-    expect(complete_header, f"Некорректный текст хедера. ОР: {complete_header_text}, "
-                            f"ФР: {complete_header.inner_text()}").to_have_text(complete_header_text)
-    expect(complete, f"Некорректный текст сообщения. ОР: {complete_text}, "
-                     f"ФР: {complete.inner_text()}").to_have_text(complete_text)
+class FinishPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.app_logo = page.locator("div.app_logo")
+        self.title = page.locator("span.title")
+        self.img = page.locator("img.pony_express")
+        self.complete_header = page.locator("h2.complete-header")
+        self.complete = page.locator("div.complete-text")
+        self.back_home_button = page.locator("button#back-to-products")
+
+    def check_elements(self):
+        """Проверка элементов страницы"""
+        expect(self.app_logo, "Не отображается текст логотипа").to_be_visible()
+        expect(self.title, "Не отображается заголовок").to_be_visible()
+        expect(self.img, "Не отображается изображение").to_be_visible()
+        expect(self.complete_header, "Не отображается сообщение в заголовке").to_be_visible()
+        expect(self.complete, "Не отображается текст об успешном заказе").to_be_visible()
+        expect(self.back_home_button, "Не отображается кнопка Back Home").to_be_visible()
+        expect(self.app_logo, f"Некорректный текст логотипа. ОР: {header_text}, "
+                              f"ФР: {self.app_logo.inner_text()}").to_have_text(header_text)
+        expect(self.title, f"Некорректный текст заголовка. ОР: {final_title}, "
+                           f"ФР: {self.title.inner_text()}").to_have_text(final_title)
+        expect(self.complete_header, f"Некорректный текст хедера. ОР: {complete_header_text}, "
+                                     f"ФР: {self.complete_header.inner_text()}").to_have_text(complete_header_text)
+        expect(self.complete, f"Некорректный текст сообщения. ОР: {complete_message}, "
+                              f"ФР: {self.complete.inner_text()}").to_have_text(complete_message)
--- tests/test_finish_page.py
+++ tests/test_finish_page.py
@@ -1,29 +1,33 @@
 import pytest
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 from vars import (url_auth_page, url_main_page, url_cart_page, url_checkout_step_1_page, url_checkout_step_2_page,
                   url_checkout_complete_page)
-from pages.auth_page import authorization
+from pages.auth_page import AuthPage
+from pages.checkout_step_1_page import CheckoutStepOnePage
+from pages.finish_page import FinishPage
 from credentials import all_users, standard_user
-from pages.checkout_step_1_page import set_inputs_and_continue
-from pages.finish_page import check_elements
 
-
+@pytest.mark.new
 @pytest.mark.finish
 @pytest.mark.parametrize(*all_users)
-def test_finish_check_elements_empty(page: Page, login, password):
+def test_finish_check_elements_empty(page, username, password):
     """Проверка элементов финальной страницы оформления заказа"""
-    page.goto(url_auth_page)
-    authorization(page, login, password)
+    auth_page = AuthPage(page)
+    auth_page.navigate()
+    assert page.url == url_auth_page, f"Некорректный адрес. ОР: {url_auth_page}, ФР: {page.url}"
+    auth_page.authorization(username, password)
     assert page.url == url_main_page, f"Некорректный адрес. ОР: {url_main_page}, ФР: {page.url}"
     page.locator("a.shopping_cart_link").click()
     assert page.url == url_cart_page, f"Некорректный адрес. ОР: {url_cart_page}, ФР: {page.url}"
-    page.locator("#checkout").click()
+    page.locator("button#checkout").click()
     assert page.url == url_checkout_step_1_page, f"Некорректный адрес. ОР: {url_checkout_step_1_page}, ФР: {page.url}"
-    set_inputs_and_continue(page)
+    checkout_step_1_page = CheckoutStepOnePage(page)
+    checkout_step_1_page.set_inputs_and_continue()
     assert page.url == url_checkout_step_2_page, f"Некорректный адрес. ОР: {url_checkout_step_2_page}, ФР: {page.url}"
     page.locator("button#finish").click()
-    assert page.url == url_checkout_complete_page, (f"Некорректный адрес. "
-                                                    f"ОР: {url_checkout_complete_page}, ФР: {page.url}")
-    check_elements(page)
+    assert page.url == url_checkout_complete_page, (f"Некорректный адрес. ОР: {url_checkout_complete_page}, "
+                                                    f"ФР: {page.url}")
+    finish_page = FinishPage(page)
+    finish_page.check_elements()
     page.locator("button#back-to-products").click()
     assert page.url == url_main_page, f"Некорректный адрес. ОР: {url_main_page}, ФР: {page.url}"
